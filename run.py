c88ce0caefa7b7a87ed28e5b2033ff4fa3752dde
WIDIANNE_DALVALLEN <widiannedalvallen@gmail.com>
2023-12-04 17:22:14 +0100

    []

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-def 
+
+
+# grid
+# placing ships, 
+# taking user input
+# handling game logic
